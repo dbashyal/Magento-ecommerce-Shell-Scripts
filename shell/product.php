73a6f8403eb1683c157b2d7fae47ce7c1db0b0cd
damu <damodar@acidgreen.com.au>
2013-09-25 11:04:11 +1000

    included not visible individually to fix as well

--- shell/product.php
+++ shell/product.php
@@ -12,6 +12,7 @@ class Mage_Shell_fixproduct extends Mage_Shell_Abstract
     public function run()
     {
         $_products = array();
+        $i = 1;
         $conn = Mage::getSingleton('core/resource')->getConnection('core_read');
         $results = $conn->fetchAll("
                 SELECT v.entity_id, v.value, count(*) as count
@@ -22,16 +23,14 @@ class Mage_Shell_fixproduct extends Mage_Shell_Abstract
                 v.attribute_id = 86
                 and
                 i.entity_id = v.entity_id
-                and
+                /*and
                 i.attribute_id=91
                 and
-                i.value > 1
+                i.value > 1*/
                 Group by value
                 having count > 1
                 ");
         foreach($results as $row) {
-            //print_r($row);
-            //echo "SELECT `entity_id` FROM `catalog_product_entity_varchar` where `store_id` = 2 and ((`attribute_id` = 86 and value = '{$row[value]}') or (`attribute_id` = 87 and value = '{$row[value]}.html'))\n";
             $entities = $conn->fetchAll("SELECT `entity_id` FROM `catalog_product_entity_varchar` where `store_id` = 2 and ((`attribute_id` = 86 and value = '{$row[value]}') or (`attribute_id` = 87 and value = '{$row[value]}.html'))");
             foreach($entities as $entity_id) {
                 $_products[$entity_id['entity_id']] = $entity_id['entity_id'];
@@ -66,7 +65,7 @@ class Mage_Shell_fixproduct extends Mage_Shell_Abstract
                 $_product->setUrlPath($urlPath);
                 $_product->setUrlKey($urlKey);
                 $_product->save();
-                echo $urlPath . " - SAVED!!! \n";
+                echo ($i++) . '> ' . $urlPath . " - SAVED!!! \n";
             }
         }
     }
