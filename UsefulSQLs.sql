8b7ee15e4c579d33ae9d8e209d49b3924a2bb062
Damodar Bashyal <dbashyal@gmail.com>
2015-11-11 16:42:20 +1100

    Create UsefulSQLs.sql

---
+++ UsefulSQLs.sql
@@ -0,0 +1,21 @@
+View current widget settings.
+```
+select pl.page_id, pl.layout_update_id, p.instance_id, i.package_theme, l.package, l.theme, i.store_ids, i.title, p.block_reference, p.layout_handle from 
+	widget_instance i,
+	core_layout_link l,
+	widget_instance_page p,
+	widget_instance_page_layout pl
+where 
+	pl.page_id = p.page_id
+	and
+	pl.layout_update_id = l.layout_update_id
+	and
+	p.instance_id = i.instance_id
+	and
+	i.package_theme not like '%mvau%'
+	and
+	i.package_theme not like '%mobile%'
+	and
+	i.package_theme not like '%enterprise/default%'
+	;
+	```
